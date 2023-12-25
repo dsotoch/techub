9a8466e0351b039160d292b49a7920823c69691b
dsotoch <dsoto6155@gmail.com>
2023-12-24 22:30:17 -0500

    33

--- Fronteend/techub/next.config.js
+++ Fronteend/techub/next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    async rewrites() {
-        return [
-            // Desactiva la generación para ciertas rutas
-            {
-                source: '/:path*',   // Coincide con cualquier ruta y cualquier subruta
-                destination: '/_error',  // Redirige a la página de error
-            },
-        ];
-    },
+   
 
 }
 
