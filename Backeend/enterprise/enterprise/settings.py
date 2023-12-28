eb12c2c7356998fe45dc4734c83b986c184c6004
dsotoch <dsoto6155@gmail.com>
2023-12-27 20:44:25 -0500

    techub

--- Backeend/enterprise/enterprise/settings.py
+++ Backeend/enterprise/enterprise/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_10zu=ah+#i*5+tz0c&5d3qym_62x2g%6&uf#hyu*%-tt=^_5z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','viru-tec.com']
 
 
 # Application definition
