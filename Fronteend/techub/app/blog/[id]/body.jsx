94ceaa0522815a3c88bbd5d0bb4834c9f8c5e44b
dsotoch <dsoto6155@gmail.com>
2023-12-27 22:24:29 -0500

    production

--- Fronteend/techub/app/blog/[id]/body.jsx
+++ Fronteend/techub/app/blog/[id]/body.jsx
@@ -3,35 +3,39 @@ import Image from "next/image";
 import React from "react";
 
 export const Body = async ({ id }) => {
+    if (process.env.NODE_ENV === 'production') {
+        const request = await fetch(process.env.API_URL + '/Blog/getBlog/' + id);
+        const response = await request.json();
+        const blog = response.blog;
+        return (
+            <Paper className="grid w-full p-4">
+                <Typography className="text-green-500 text-3xl m-2 italic">Somos TecHub</Typography>
+                <Typography className="ml-2 mb-2 text-cyan-900 font-semibold">Más de 3 años en TI</Typography>
+                <hr />
+                <div className="p-2">
+                    <div className="w-full m-2 p-2">
+                        <Typography variant="h4" className="text-cyan-900">{blog.titulo}</Typography>
+                        <Typography className="text-2xl font-semibold italic">{blog.subtitulo}</Typography>
+                        <Typography className="mt-2 text-cyan-900 text-justify">{blog.descripcion}</Typography>
+                        <div className="w-full border p-2 m-2">
+                            <Image src={`${process.env.API_URL}${blog.imagen}`} alt="" srcSet="" title={blog.titulo} className="object-cover w-full" />
+                        </div>
+                        < div className="mt-5 text-center">
+                            {blog.link != null && (
+                                <a href={blog.link} className="p-3 m-2 bg-blue-600 hover:bg-blue-400 text-white" target="_blank" title={blog.titulo} rel="noopener noreferrer">
+                                    Ir A {blog.titulo}
+                                </a>
+                            )}
+                        </div>
+                    </div>
 
-    const request = await fetch(process.env.API_URL + '/Blog/getBlog/' + id);
-    const response = await request.json();
-    const blog = response.blog;
-    return (
-        <Paper className="grid w-full p-4">
-            <Typography className="text-green-500 text-3xl m-2 italic">Somos TecHub</Typography>
-            <Typography className="ml-2 mb-2 text-cyan-900 font-semibold">Más de 3 años en TI</Typography>
-            <hr />
-            <div className="p-2">
-                <div className="w-full m-2 p-2">
-                    <Typography variant="h4" className="text-cyan-900">{blog.titulo}</Typography>
-                    <Typography className="text-2xl font-semibold italic">{blog.subtitulo}</Typography>
-                    <Typography className="mt-2 text-cyan-900 text-justify">{blog.descripcion}</Typography>
-                    <div className="w-full border p-2 m-2">
-                        <Image src={`${process.env.API_URL}${blog.imagen}`} alt="" srcSet="" title={blog.titulo} className="object-cover w-full" />
-                    </div>
-                    < div className="mt-5 text-center">
-                        {blog.link != null && (
-                            <a href={blog.link} className="p-3 m-2 bg-blue-600 hover:bg-blue-400 text-white" target="_blank" title={blog.titulo} rel="noopener noreferrer">
-                                Ir A {blog.titulo}
-                            </a>
-                        )}
-                    </div>
+
+
                 </div>
+            </Paper>
+        );
+    } else {
+        return (<div></div>);
+    }
 
-
-
-            </div>
-        </Paper>
-    );
 };
--- Fronteend/techub/app/blog/body.jsx
+++ Fronteend/techub/app/blog/body.jsx
@@ -2,31 +2,36 @@ import { Card, CardContent, Paper, Typography, CircularProgress, Button } from "
 import Link from "next/link";
 import React from "react";
 export async function Body() {
-    const apiUrl = process.env.API_URL || 'http://localhost:8080';
-    const endpoint = 'Blog';
-    const response = await fetch(apiUrl + '/' + endpoint);
-    const res = await response.json();
-    return (
-        <Paper className="grid w-full md:p-4 p-2">
-            <Typography className="text-green-500 text-3xl m-2 italic">Somos TecHub</Typography>
-            <Typography className="ml-2 mb-2 text-cyan-900 font-semibold">Más de 3 años en TI</Typography>
-            <div className="md:flex grid w-full justify-around">
-                {res.map((blog) => (
-                    <Card className="p-4 border w-full md:w-[30%] ml-2 md:mt-0 mt-2" elevation={5} key={blog.id}>
-                        <CardContent className=" text-cyan-900">
-                            <Typography className="font-semibold text-2xl italic my-2">{blog.titulo}</Typography>
-                            <Typography className="font-semibold">{blog.subtitulo}</Typography>
-                            <Typography>{blog.fecha}</Typography>
-                            <Typography className="text-green-500 italic font-semibold">{blog.autor}</Typography>
-                            <div className="mt-5 text-center">
-                                <Link href={`blog/${blog.id}`} className="bg-gray-900 p-3 rounded-md hover:bg-gray-500 text-white">Ver Nota Completa</Link>
-                            </div>
-                        </CardContent>
-                    </Card>
-                ))
-                }
-            </div>
+    if (process.env.NODE_ENV === 'production') {
+        const apiUrl = process.env.API_URL;
+        const endpoint = 'Blog';
+        const response = await fetch(apiUrl + '/' + endpoint);
+        const res = await response.json();
+        return (
+            <Paper className="grid w-full md:p-4 p-2">
+                <Typography className="text-green-500 text-3xl m-2 italic">Somos TecHub</Typography>
+                <Typography className="ml-2 mb-2 text-cyan-900 font-semibold">Más de 3 años en TI</Typography>
+                <div className="md:flex grid w-full justify-around">
+                    {res.map((blog) => (
+                        <Card className="p-4 border w-full md:w-[30%] ml-2 md:mt-0 mt-2" elevation={5} key={blog.id}>
+                            <CardContent className=" text-cyan-900">
+                                <Typography className="font-semibold text-2xl italic my-2">{blog.titulo}</Typography>
+                                <Typography className="font-semibold">{blog.subtitulo}</Typography>
+                                <Typography>{blog.fecha}</Typography>
+                                <Typography className="text-green-500 italic font-semibold">{blog.autor}</Typography>
+                                <div className="mt-5 text-center">
+                                    <Link href={`blog/${blog.id}`} className="bg-gray-900 p-3 rounded-md hover:bg-gray-500 text-white">Ver Nota Completa</Link>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    ))
+                    }
+                </div>
 
-        </Paper>
-    );
+            </Paper>
+        );
+    } else {
+        return (<div></div>)
+    }
+
 }
