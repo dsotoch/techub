bb91ed0cac66d9092807d21cee97d009246bd34d
dsotoch <dsoto6155@gmail.com>
2024-01-20 23:15:09 -0500

    ok

--- Backeend/techub/resources/views/welcome.blade.php
+++ Backeend/techub/resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" href="{{asset('images/logo_pequeño.png')}}" type="image/x-icon">
     <title>@yield('titulo')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    @vite('resources/css/app.css')
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('estilos')
 </head>
 
