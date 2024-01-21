c86d1753a5f7c9c4941af306308d74c51ac1164c
dsotoch <dsoto6155@gmail.com>
2024-01-20 22:34:46 -0500

    ok

--- Backeend/techub/vite.config.js
+++ Backeend/techub/vite.config.js
@@ -9,8 +9,11 @@ export default defineConfig({
         }),
     ],
     server: {
-        
+        https: {
+          key: '/etc/letsencrypt/live/back-techub.viru-tec.com/privkey.pem',
+          cert: '/etc/letsencrypt/live/back-techub.viru-tec.com/fullchain.pem'
+        },
         host: '0.0.0.0',
         port: 5173,
-      },
+      }
 });
